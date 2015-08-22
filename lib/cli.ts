2439541d4e14ed94d296f8dcc8c4153dc2783a42
vvakame <vvakame+dev@gmail.com>
2015-08-22 17:50:32 +0900

    change commander to commandpost

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,58 +1,75 @@
+/// <reference path="../node_modules/typescript/bin/lib.es6.d.ts" />
 /// <reference path="../typings/node/node.d.ts" />
-/// <reference path="../typings/commander/commander.d.ts" />
 /// <reference path="../typings/js-yaml/js-yaml.d.ts" />
+/// <reference path="../node_modules/commandpost/commandpost.d.ts" />
 
-import fs = require("fs");
+import * as fs from "fs";
+import * as yaml from "js-yaml";
+import * as lib from "./index";
 
-import yaml = require("js-yaml");
-
-import lib = require("./index");
-
+import * as commandpost from "commandpost";
 var pkg = require("../package.json");
 
-var program:IExportedCommand = require("commander");
-
-interface IExportedCommand extends commander.IExportedCommand {
+interface RootOpts {
 	json:boolean;
 	yaml:boolean;
-	replace:string;
-	rules:string;
+	replace:boolean;
+	rules:string[];
 }
 
-program
+interface RootArgs {
+	files: string[];
+}
+
+let root = commandpost
+	.create<RootOpts, RootArgs>("prh <files...>")
 	.version(pkg.version, "-v, --version")
-	.usage('[options] <file ...>')
 	.option("--json", "rule set to json")
 	.option("--yaml", "rule set to parsed yaml")
 	.option("--rules <path>", "path to rule yaml file")
-	.option("-r, --replace", "replace input files");
-
-program.parse(process.argv);
-
-(()=> {
-	var paths = [__dirname + "/../misc/WEB+DB_PRESS.yml"];
-	if (program.rules) {
-		paths = program.rules.split(",");
-	}
-	var config = lib.fromYAMLFilePath(paths[0]);
-	paths.splice(1).forEach(path => {
-		var c = lib.fromYAMLFilePath(path);
-		config.merge(c);
-	});
-
-	if (program.json) {
-		console.log(JSON.stringify(config, null, 2));
-		return;
-	} else if (program.yaml) {
-		console.log(yaml.dump(JSON.parse(JSON.stringify(config, null, 2))));
-		return;
-	}
-	program.args.forEach(filePath => {
-		var result = config.replaceByRule(filePath);
-		if (program.replace) {
-			fs.writeFileSync(filePath, result);
-		} else {
-			console.log(result);
+	.option("-r, --replace", "replace input files")
+	.action((opts, args) => {
+		console.log(opts, args);
+		var paths = [__dirname + "/../misc/WEB+DB_PRESS.yml"];
+		if (opts.rules) {
+			paths = opts.rules;
 		}
+		var config = lib.fromYAMLFilePath(paths[0]);
+		paths.splice(1).forEach(path => {
+			var c = lib.fromYAMLFilePath(path);
+			config.merge(c);
+		});
+
+		if (opts.json) {
+			console.log(JSON.stringify(config, null, 2));
+			return;
+		} else if (opts.yaml) {
+			console.log(yaml.dump(JSON.parse(JSON.stringify(config, null, 2))));
+			return;
+		}
+		args.files.forEach(filePath => {
+			var result = config.replaceByRule(filePath);
+			if (opts.replace) {
+				fs.writeFileSync(filePath, result);
+			} else {
+				console.log(result);
+			}
+		});
 	});
-})();
+
+commandpost
+	.exec(root, process.argv)
+	.catch(errorHandler);
+
+function errorHandler(err:any) {
+	"use strict";
+
+	if (err instanceof Error) {
+		console.error(err.stack);
+	} else {
+		console.error(err);
+	}
+	return Promise.resolve(null).then(()=> {
+		process.exit(1);
+	});
+}
