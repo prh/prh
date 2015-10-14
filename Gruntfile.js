b6f4511d3e37821239d7aba18d7a9739b63cb313
vvakame <vvakame+dev@gmail.com>
2015-10-14 22:56:43 +0900

    feat(build): add grunt-conventional-changelog

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,6 +99,29 @@ module.exports = function (grunt) {
                     '<%= opt.client.jsTestOut %>/**/*Spec.js'
                 ]
             }
+        },
+        conventionalChangelog: {
+            options: {
+                changelogOpts: {
+                    // conventional-changelog options go here
+                    preset: "angular"
+             },
+             context: {
+                    // context goes here
+             },
+             gitRawCommitsOpts: {
+                    // git-raw-commits options go here
+             },
+             parserOpts: {
+                    // conventional-commits-parser options go here
+             },
+             writerOpts: {
+                    // conventional-changelog-writer options go here
+             }
+            },
+            release: {
+                src: "CHANGELOG.md"
+            }
         }
     });
 
