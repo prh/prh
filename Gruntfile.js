396fd6f8ea269065e058ca2cb2dc7ef7acf3ac62
vvakame <vvakame+dev@gmail.com>
2017-05-04 23:16:15 +0900

    feat(deps): 脱gruntしていく

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,8 @@
+"use strict";
+
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        opt: {
-            client: {
-                "tsMain": "lib",
-                "tsTest": "test",
-
-                "jsMainOut": "lib",
-                "jsTestOut": "test"
-            }
-        },
-
-		exec: {
-			tsc: "tsc -p ./",
-            tsfmt: "tsfmt -r"
-		},
-        tslint: {
-            options: {
-                configuration: grunt.file.readJSON("tslint.json")
-            },
-            files: {
-                src: [
-                    '<%= opt.client.tsMain %>/**/*.ts',
-                    '<%= opt.client.tsTest %>/**/*.ts',
-                    '!<%= opt.client.tsMain %>/**/*.d.ts'
-                ]
-            }
-        },
-        typedoc: {
-            main: {
-                options: {
-                    module: "<%= ts.options.module %>",
-                    out: './docs',
-                    name: '<%= pkg.name %>',
-                    target: "<%= ts.options.target %>"
-                },
-                src: [
-                    '<%= opt.client.tsMain %>/**/*.ts'
-                ]
-            }
-        },
-        clean: {
-            clientScript: {
-                src: [
-                    // client
-                    '<%= opt.client.jsMainOut %>/*.js',
-                    '<%= opt.client.jsMainOut %>/*.d.ts',
-                    '<%= opt.client.jsMainOut %>/*.js.map',
-                    // client test
-                    '<%= opt.client.jsTestOut %>/*.js',
-                    '<%= opt.client.jsTestOut %>/*.js.map',
-                    '<%= opt.client.jsTestOut %>/*.d.ts'
-                ]
-            }
-        },
-        mochaTest: {
-            test: {
-                options: {
-                    reporter: 'spec',
-                    timeout: 60000,
-                    require: [
-                        function () {
-                            require('espower-loader')({
-                                cwd: process.cwd() + '/' + grunt.config.get("opt.client.jsTestOut"),
-                                pattern: '**/*.js'
-                            });
-                        }
-                    ]
-                },
-                src: [
-                    '<%= opt.client.jsTestOut %>/**/*Spec.js'
-                ]
-            }
-        },
         conventionalChangelog: {
             options: {
                 changelogOpts: {
@@ -97,14 +27,6 @@ module.exports = function (grunt) {
             }
         }
     });
-
-    grunt.registerTask(
-        'default',
-        ['exec:tsfmt', 'exec:tsc', 'tslint']);
-
-    grunt.registerTask(
-        'test',
-        ['default', 'mochaTest']);
 
     require('load-grunt-tasks')(grunt);
 };
