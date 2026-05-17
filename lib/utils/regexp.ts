09d2dc821119b9ad125c6230ba3e13b7dfd798f9
Masataka Yakura <masataka.yakura@gmail.com>
2026-05-17 23:17:23 +0900

    fix(regexp): don't escape hyphen in escapeSpecialChars (Issue #34) (#95)
    Hyphen (-) is only special inside character classes in JS regex.
    Escaping it outside [] with u flag causes SyntaxError.
    Also added tests to verify hyphen works with gmu flags.

--- lib/utils/regexp.ts
+++ lib/utils/regexp.ts
@@ -114,7 +114,7 @@ export function addDefaultFlags(regexp: RegExp) {
 // Note: Node.js v24がターゲット下限になったらRegExp.escape()に置き換えられる
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/escape
 export function escapeSpecialChars(str: string): string {
-    return str.replace(/[\\*+.?{}()|^$[\]/-]/g, "\\$&");
+    return str.replace(/[\\*+.?{}()|^$[\]/]/g, "\\$&");
 }
 
 export function equals(a: RegExp, b: RegExp) {
